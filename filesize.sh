83cf1be6686f9852de596932330f7b22fd66f4c2
G-gnome <121409271+G-gnome@users.noreply.github.com>
2023-10-06 19:27:38 +0000

    message

--- filesize.sh
+++ filesize.sh
@@ -36,37 +36,3 @@ python -c "print($COMPRESSED / $UNCOMPRESSED)"
 echo "
 bye! :)"
 
-ziped=$(du -h threatened-species.csv.gz | cut -f1)
-
-echo "Zipped file size: $ziped
-"
-
-gunzip -k -f threatened-species.csv.gz
-
-unziped=$(du -h threatened-species.csv | cut -f1)
-
-echo "Unzipped file size: $unziped
-"
-
-wordcount=$(wc -l threatened-species.csv)
-
-echo "Lines in unziped file: 
-$wordcount
-"
-
-echo "what I get when I do gzip -l threatened-species.csv.gz:
-"
-
-gzip -l threatened-species.csv.gz
-
-COMPRESSED=$(du threatened-species.csv.gz | cut -f1)
-UNCOMPRESSED=$(du threatened-species.csv | cut -f1)
-
-echo "
-what I get when I do Jason's python script
-"
-
-python -c "print($COMPRESSED / $UNCOMPRESSED)"
-
-echo "
-bye! :)"
